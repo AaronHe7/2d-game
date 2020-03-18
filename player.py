b2dcf04e9392376244e08dbe67c17f41c4fee36c
sqawkingapple <stevenning.203@gmail.com>
2020-03-18 15:38:38 -0600

    Attempting to fix gravity

--- player.py
+++ player.py
@@ -20,19 +20,19 @@ class Player:
         right = Coordinate(self.x + self.w, self.y + self.h/2, self.tilemap)
         bottom = Coordinate(self.x + self.w/2, self.y + self.h + 1, self.tilemap)
 
-        if top.get_tile() != 0:
+        if top.get_tile()[0] != 0:
             self.vy = 0
             self.y += 5
-        if left.get_tile() != 0:
+        if left.get_tile()[0] != 0:
             self.vx = 0
             self.x += 5
-        if bottom.get_tile() != 0:
+        if bottom.get_tile()[0] != 0:
             self.vy = 0
             self.ay = 0
             self.on_ground = True
-        if bottom.get_tile() == 0:
+        if bottom.get_tile()[0] == 0:
             self.ay = -1
-        if right.get_tile() != 0:
+        if right.get_tile()[0] != 0:
             self.vx = 0
             self.x -= 5
 
@@ -53,4 +53,4 @@ class Coordinate:
         col = math.floor(self.x/80)
         if row < 0 or row >= len(self.tilemap) or col < 0 or col >= len(self.tilemap[0]):
             return 1
-        return self.tilemap[math.floor(self.y/80)][math.floor(self.x/80)]
+        return [self.tilemap[math.floor(self.y/80)][math.floor(self.x/80)],[row,col]]
