8cdb01f014fec792f95905b511971af56dcd1bba
sqawkingapple <stevenning.203@gmail.com>
2020-03-18 14:33:54 -0600

    Added images for new blocks fixed

--- view.py
+++ view.py
@@ -8,8 +8,11 @@ display = pygame.display.set_mode((width,height))
 
 tilesize = 80
 sky = pygame.image.load("sky.png")
+sky = pygame.transform.scale(sky, (tilesize, tilesize))
 grass_block = pygame.image.load("grass_block.png")
+grass_block = pygame.transform.scale(grass_block, (tilesize, tilesize))
 dirt_block = pygame.image.load("dirt_block.png")
+dirt_block = pygame.transform.scale(dirt_block, (tilesize, tilesize))
 
 textures = [sky, grass_block, dirt_block]
 tilemap = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
