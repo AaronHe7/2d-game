8020f779283ad65b4788d5425547976c791b4504
Aaron He <aaronhe07@gmail.com>
2020-03-19 16:16:26 -0700

    Fix error with jumping

--- player.py
+++ player.py
@@ -20,9 +20,10 @@ class Player:
         self.y -= self.vy
         self.vy += self.ay
         self.on_ground = False
+        points_touching_ground = 0
         
         # Number of hitbox points on each side of the character
-        hitbox_points = 10
+        hitbox_points = 20
         for i in range(1, hitbox_points):
             # The coordinates that vary as i varies
             x_variation_coord = self.x + i * self.w/hitbox_points
@@ -31,9 +32,7 @@ class Player:
             top = Coordinate(x_variation_coord, self.y, self.tilemap)
             bottom = Coordinate(x_variation_coord, self.y + self.h, self.tilemap)
             left = Coordinate(self.x, y_variation_coord, self.tilemap)
-            right = Coordinate(self.x + self.w, y_variation_coord, self.tilemap)
-
-            
+            right = Coordinate(self.x + self.w - 1, y_variation_coord, self.tilemap)
             
             if top.get_tile() != 0:
                 self.vy = 0
@@ -41,19 +40,21 @@ class Player:
             if left.get_tile() != 0:
                 self.vx = 0
                 self.x += player_speed
-            if bottom.get_tile() != 0:
-                if self.on_ground:
-                    self.vy = 0
-                self.on_ground = True
-                self.ay = 0
-                self.y = (bottom.get_row_col()['r'] - self.h/tilesize) * tilesize
-            
+            if bottom.get_tile() != 0:                   
+                points_touching_ground += 1
             if right.get_tile() != 0:
                 self.vx = 0
                 self.x -= player_speed
 
         if not self.on_ground:
             self.ay = gravity
+            
+        # Player can jump again only if more than 1/5 of the player is touching the ground
+        if points_touching_ground >= hitbox_points/5:
+            self.y = (bottom.get_row_col()['r'] - self.h/tilesize) * tilesize
+            self.ay = 0
+            self.vy = 0
+            self.on_ground = True
 
         
 
